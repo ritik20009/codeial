470a7d5c0ec02601907ac716732da59509d10ff0
ritik <ritikozib189@gmail.com>
2022-03-23 15:55:53 +0530

    populating the db

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -4,22 +4,21 @@ module.exports.home = function(req, res){
     // console.log(req.cookies);
     // res.cookie('user_id', 25);
 
-    Post.find({}, function(err,posts){
-        
-        return res.render('home', {
-            title: "Codeial || Home",
-            posts: posts
-        });
-    });
-
-    // Post.find({}).populate('user').exec(function(err,posts){
-
+    // Post.find({}, function(err, posts){
     //     return res.render('home', {
-    //         title: "Codeial || Home",
-    //         posts: posts
+    //         title: "Codeial | Home",
+    //         posts:  posts
     //     });
     // });
-   
-}
 
-// module.exports.actionName = function(req, res){}
+    // populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
+
+        console.log(posts);
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts:  posts
+        });
+    })
+
+}
