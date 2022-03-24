b17a63c008c72971e678d215576967672565f25f
ritik <ritikozib189@gmail.com>
2022-03-24 11:15:11 +0530

    created the functionality that display the user name who created this post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -11,14 +11,13 @@ module.exports.home = function(req, res){
     //     });
     // });
 
-    // populate the user of each post
+    //populate the user of each post
     Post.find({}).populate('user').exec(function(err, posts){
-
-        console.log(posts);
+        
         return res.render('home', {
             title: "Codeial | Home",
             posts:  posts
         });
-    })
+    });
 
 }
