07c707920ef934a2b5c8e6e73ac0be70206e988d
ritik <ritikozib189@gmail.com>
2022-03-27 14:33:09 +0530

    created partials for displaying posts and comments

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,5 +1,6 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
+const { post } = require('../routes/comments');
 module.exports.create = function(req,res){
 
     Post.findById(req.body.post,function(err,post){
@@ -27,6 +28,27 @@ module.exports.create = function(req,res){
             });
 
            
+        }
+    });
+}
+
+module.exports.destroy = function(req,res){
+
+    Comment.findById(req.params.id, function(err,comment){
+
+
+        if(comment.user == req.user.id){
+
+            let postid = comment.post;
+
+            comment.remove();
+
+            Post.findByIdAndUpdate(postid, { $pull: {comments: req.params.id}}, function(err,post){
+
+                return res.redirect('back');
+            });
+        } else{
+            return res.redirect('back');
         }
     });
 }
