b8199bbd6ce14f327371179cc6b265a56c964cd6
ritik <ritikozib189@gmail.com>
2022-04-03 17:04:48 +0530

    showing the profile picture

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -13,18 +13,6 @@ module.exports.profile = function(req, res){
 
 module.exports.update = async function(req,res){
 
-    // if(req.user.id==req.params.id){
-    //     User.findByIdAndUpdate(req.params.id, req.body, function(err,user){
-
-    //         return res.redirect('back');
-    //     });
-    // }
-    // else{
-
-    //      req.flash('error', 'UnAuthorized');
-    //     return res.status(401).send('Unauthorized');
-    // }
-
     if(req.user.id==req.params.id){
 
         try{
