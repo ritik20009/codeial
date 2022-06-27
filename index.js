435d80b53fe6908868e1b3527e713b92833a4f7d
ritik <ritikozib189@gmail.com>
2022-06-27 12:50:43 +0530

    improved

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const app = express();
 require('./config/view-helpers')(app);
-const port = process.env.PORT || 8000;
+const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 // used for session cookie
